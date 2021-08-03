10a36b3d05b5f04d7b145005038f67ee72965ef8
ratang <amianijohns@gmail.com>
2021-08-03 19:58:46 -0400

    remove extraneous files

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,7 @@
-import React, { forwardRef, useEffect, useRef } from "react"
+import React, { useEffect } from 'react'
 import "./App.css"
-import { Canvas, CanvasRef } from "./Canvas"
 import Game from './Game'
-import { useWindowSize } from "./hooks/useWindowSize"
 import { world } from "./world"
-import * as PIXI from 'pixi.js'
-
-const Render = forwardRef<CanvasRef>(function Render(props, ref) {
-  const size = useWindowSize()
-  return <Canvas {...size} ref={ref} />
-})
 
 function App() {
   useEffect(() => {
