504f7d7e90ffb30e8e8a3eb96c07bb5003b6c56f
ratang <amianijohns@gmail.com>
2021-08-04 11:17:32 -0400

    rearrange

--- client/src/pixiApp.ts
+++ client/src/pixiApp.ts
@@ -8,30 +8,25 @@ const app = new PIXI.Application({
 	antialias: true,
 })
 
-const viewport = new Viewport({
+const viewport = app.stage.addChild(new Viewport({
     screenWidth: app.view.width,
     screenHeight: app.view.height,
     worldWidth: 1000,
     worldHeight: 1000,
 
     interaction: app.renderer.plugins.interaction // the interaction module is important for wheel to work properly when renderer.view is placed or scaled
-})
+}))
 
-app.stage.addChild(viewport)
 
 // activate plugins
 viewport
     .drag()
     .wheel()
     .decelerate()
-app.loader.add('ship', ship).load()
 
-/*
-const dot = viewport.addChild(new PIXI.Graphics())
-dot.beginFill(0xffffff)
-dot.drawCircle(0, 0, 20)
-*/
 viewport.fit()
 viewport.moveCenter(0, 0)
+
+app.loader.add('ship', ship).load()
 
 export { app, viewport }
