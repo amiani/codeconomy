e0d49cd519d309a8c5c4aca53046710e4c1f7f62
ratang <amianijohns@gmail.com>
2021-08-08 00:54:37 -0400

    add basic editor

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,12 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import "./App.css"
 import Game from './Game'
 import { world } from "./world"
+import Editor from 'react-simple-code-editor'
+import { highlight, languages } from 'prismjs'
+import 'prismjs/themes/prism.css'
+import 'prismjs/components/prism-clike';
+import 'prismjs/components/prism-javascript';
 
 function App() {
   useEffect(() => {
@@ -20,12 +25,32 @@ function App() {
     }
   }, [])
 
+const [code, setCode] = useState(`function add(a, b) {
+  return a + b;
+}
+`)
+
   return (
     <div className="App">
       <Game />
+      <Editor
+        value={code}
+        onValueChange={setCode}
+        highlight={code => highlight(code, languages.js, 'js')}
+        padding={10}
+        style={{
+          fontFamily: '"Fira code", "Fira Mono", Consolas, Menlo, monospace',
+          fontSize: 12,
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          background: 'white',
+        }}
+      />
+
     </div>
   )
 }
 
-export default App
 ;(window as any).world = world
+export default App
