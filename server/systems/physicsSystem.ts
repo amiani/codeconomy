022bada80571fb1616ed517f2290e06262d9b397
ratang <amianijohns@gmail.com>
2021-08-09 10:51:24 -0400

    remove createLaser

--- server/systems/physicsSystem.ts
+++ server/systems/physicsSystem.ts
@@ -18,43 +18,9 @@ import {
 	Action,
 	Bullet
 } from '../components'
+import createLaser from '../createLaser'
 
 const bodiesActionTeamWeapon = createQuery(Body, Action, Team, Weapon)
-
-const createLaser = (
-	world: World,
-	position: { x: number; y: number },
-	rotation: number,
-	team: number
-) => {
-	const e = world.create()
-	const speed = 100
-	const velocity = { x: Math.cos(rotation) * speed, y: Math.sin(rotation) * speed }
-	const bodyDesc = rapier.RigidBodyDesc.newKinematicVelocityBased()
-		.setTranslation(position.x, position.y)
-		.setRotation(rotation)
-		.setLinvel(velocity.x, velocity.y)
-
-	const colliderDesc = rapier.ColliderDesc.cuboid(16, 2)
-		.setCollisionGroups(0x00040000 * (team + 1) + 2 - team)
-		.setIsSensor(true)
-		.setActiveEvents(rapier.ActiveEvents.INTERSECTION_EVENTS)
-
-	const sim = useSimulation()
-	const body = sim.createRigidBody(bodyDesc)
-	//console.log(`New bullet created: ${e}: ${body.handle}`)
-	const collider = sim.createCollider(colliderDesc, body.handle)
-	const colliderToEntity = useColliderToEntity()
-	colliderToEntity.set(collider.handle, e)
-
-	world.attach(e,
-		toComponent(body, Body),
-		component(Transform, position),
-		component(Team, { id: team }),
-		component(Bullet, { velocity, lifetime: 1, damage: 1 }),
-		component(SpriteData, { name: 'smallbluelaser' })
-	)
-}
 
 const bodies = createQuery(Body)
 export default function physicsSystem(world: World) {
