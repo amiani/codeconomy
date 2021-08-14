b9f3dd2b30bc5a8fa5a8788aaeac6e2c3258f125
ratang <amianijohns@gmail.com>
2021-08-14 11:15:33 -0400

    listen on varying interfaces in dev/prod

--- server/env.ts
+++ server/env.ts
@@ -1,5 +1,6 @@
 export const TICK_RATE = Number(process.env.TICK_RATE) || 60
 export const SEND_RATE = Number(process.env.SEND_RATE) || 10
 export const PORT = Number(process.env.PORT) || 8000
+export const INTERFACE = process.env.INTERFACE || 'localhost'
 export const MESSAGE_MAX_BYTE_LENGTH =
   Number(process.env.MESSAGE_MAX_BYTE_LENGTH) || Infinity
--- server/index.ts
+++ server/index.ts
@@ -1,11 +1,17 @@
 import { createHrtimeLoop } from "@javelin/hrtime-loop"
-import { PORT, TICK_RATE } from "./env"
+import { PORT, TICK_RATE, INTERFACE } from "./env"
 import { fastify } from "./net"
 import { world } from "./world"
 import * as admin from 'firebase-admin'
 
-createHrtimeLoop(world.step, (1 / TICK_RATE) * 1000).start()
 admin.initializeApp({
     credential: admin.credential.applicationDefault(),
 })
-fastify.listen(PORT)
+createHrtimeLoop(world.step, (1 / TICK_RATE) * 1000).start()
+fastify.listen(PORT, INTERFACE, function (err, address) {
+    if (err) {
+      fastify.log.error(err)
+      process.exit(1)
+    }
+    console.log(`server listening on ${address}`)
+  })
