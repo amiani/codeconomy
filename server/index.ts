5716fb80db002e4b8980bb096a41a6db3ead3168
ratang <amianijohns@gmail.com>
2021-08-14 23:52:40 -0400

    add agones

--- server/index.ts
+++ server/index.ts
@@ -1,15 +1,30 @@
 import { createHrtimeLoop } from "@javelin/hrtime-loop"
 import { PORT, TICK_RATE, INTERFACE } from "./env"
-import { fastify } from "./net"
 import { world } from "./world"
 import * as admin from 'firebase-admin'
 //@ts-ignore
 import geckos from './geckosServer'
+const AgonesSDK = require('@google-cloud/agones-sdk')
+
+const agonesSDK = new AgonesSDK()
 
 admin.initializeApp({
     credential: admin.credential.applicationDefault(),
-})
-createHrtimeLoop(world.step, (1 / TICK_RATE) * 1000).start()
+});
+
+(async () => {
+  if (process.env.NODE_ENV === 'production') {
+    await agonesSDK.connect()
+    const result = await agonesSDK.ready()
+    const healthInterval = setInterval(() => {
+      agonesSDK.health();
+      console.log('Health ping sent');
+    }, 20000);
+  }
+  createHrtimeLoop(world.step, (1 / TICK_RATE) * 1000).start()
+})()
+
+
 /*
 fastify.listen(PORT, INTERFACE, function (err, address) {
     if (err) {
