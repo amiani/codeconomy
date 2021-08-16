a1ce6eb5e7945e71f9a960caad9e93c01edfb246
ratang <amianijohns@gmail.com>
2021-08-16 16:33:54 -0400

    organize

--- server/factories/createPlayer.ts
+++ server/factories/createPlayer.ts
@@ -6,10 +6,6 @@ import { Player, Isolate } from "../components"
 export default function createPlayer(world: World<Clock>, uid: string) {
     const isolate = new ivm.Isolate({ memoryLimit: 128 })
     const player = world.create(toComponent(isolate, Isolate))
-    world.attach(player, component(Player, {
-      uid,
-      name: uid,
-      //spawners: [spawner]
-    }))
-	return player
+    world.attach(player, component(Player, { uid, name: uid }))
+    return player
 }
