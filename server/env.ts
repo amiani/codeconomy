af8bca3cff8327288473a3b2945a2ba3bc22618f
ratang <amianijohns@gmail.com>
2021-08-20 11:44:36 -0400

    reduce maximum packet size

--- server/env.ts
+++ server/env.ts
@@ -2,6 +2,5 @@ export const TICK_RATE = Number(process.env.TICK_RATE) || 60
 export const SEND_RATE = Number(process.env.SEND_RATE) || 10
 export const PORT = Number(process.env.PORT) || 8000
 export const INTERFACE = process.env.NODE_ENV === 'production' ? '0.0.0.0' : '127.0.0.1'
-export const MESSAGE_MAX_BYTE_LENGTH =
-  Number(process.env.MESSAGE_MAX_BYTE_LENGTH) || Infinity
+export const MESSAGE_MAX_BYTE_LENGTH = 1446
 export const MAX_PLAYERS = 10
