e257011ba42ad6e17336a24badaac99b305e62a3
ratang <amianijohns@gmail.com>
2021-08-20 10:32:34 -0400

    update every second

--- client/src/systems/huntScoreDisplaySystem.ts
+++ client/src/systems/huntScoreDisplaySystem.ts
@@ -1,13 +1,16 @@
-import { createQuery, World } from "@javelin/ecs";
+import { createQuery, useInterval, World } from "@javelin/ecs";
 import { HuntScore, Player } from "../../../server/components";
 import { actions, Score } from "../ui/state";
 
 const playerScores = createQuery(Player, HuntScore)
 
 export default function huntScoreDisplaySystem(world: World) {
-	const scores = Array<Score>()
-	playerScores((e, [player, score]) => {
-		scores.push({ name: player.name, points: score.points })
-	})
-	actions.setScores(scores)
+	const update = useInterval(1000)
+	if (update) {
+		const scores = Array<Score>()
+		playerScores((e, [player, score]) => {
+			scores.push({ name: player.name, points: score.points })
+		})
+		actions.setScores(scores)
+	}
 }
