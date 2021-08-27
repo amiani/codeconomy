d04306310fb881a0b6b77ec57bdc2df514bc062f
ratang <amianijohns@gmail.com>
2021-08-27 19:54:23 -0400

    rename rotate to yaw

--- scripts/testScript.ts
+++ scripts/testScript.ts
@@ -2,7 +2,7 @@ export default `export default function(observation) {
   const { self, allies, enemies } = observation
   return {
     throttle: 50,
-    rotate: 0,
+    yaw: 0,
     fire: false
   }
 }`
